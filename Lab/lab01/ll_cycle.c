22aa89eea5082af13f0a18892f39e5f9e40707e8
zhumingtao <2830849787@qq.com>
2025-10-02 15:25:12 +0800

    update

--- Lab/lab01/ll_cycle.c
+++ Lab/lab01/ll_cycle.c
@@ -3,5 +3,20 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    if (head == NULL) {
+        return 0;
+    }
+
+    node *tortoise = head;
+    node *hare = head;
+
+    while (hare != NULL && hare->next != NULL) {
+        tortoise = tortoise->next;
+        hare = hare->next->next;
+
+        if (hare == tortoise) {
+            return 1;
+        }
+    }
     return 0;
 }
